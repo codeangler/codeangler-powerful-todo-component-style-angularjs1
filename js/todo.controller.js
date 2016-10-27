026f42cec17eee7b2b5c83256d7e2052df896740
codeangler <dev@codeangler.com>
2016-10-27 11:06:25 -0600

    add remove box; track total remaining; permit editing upon double click

--- js/todo.controller.js
+++ js/todo.controller.js
@@ -1,9 +1,12 @@
 (function(){
   'use-strict';
   function TodoController() {
+    
+    this.newTodo = '';
+
     this.list =[{
       'title': 'First todo Item!',
-      completed: true,
+      'completed': false,
     },
     {
       'title': 'Second todo Item!',
@@ -17,6 +20,24 @@
       'title': 'Fourth todo Item!',
       'completed': false,
     }];
+
+    this.addTodo = function() {
+      this.list.unshift({
+        'title': this.newTodo,
+        'completed': false
+      });
+      this.newTodo = '';
+    };
+
+    this.removeTodo = function (item, index) {
+      this.list.splice(index, 1);
+    };
+
+    this.getRemaining = function () {
+      return this.list.filter(function (item) { 
+        return !item.completed;
+      });
+    };
     // console.log(this.list)
   }
 
